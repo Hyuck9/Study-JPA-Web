d47403473120789a3e08cab2e158bd722904e663
Hyuck <lhg1304@gmail.com>
2021-03-28 17:38:51 +0900

    회원 가입 패스워드 암호화

--- src/main/java/com/example/studyhyuck/account/AccountService.java
+++ src/main/java/com/example/studyhyuck/account/AccountService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -14,6 +15,7 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final JavaMailSender javaMailSender;
+    private final PasswordEncoder passwordEncoder;
 
     public void processNewAccount(SignUpForm signUpForm) {
         Account newAccount = saveNewAccount(signUpForm);
@@ -25,7 +27,7 @@ public class AccountService {
         Account account = Account.builder()
                 .email(signUpForm.getEmail())
                 .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODO: encoding 해야함
+                .password(passwordEncoder.encode(signUpForm.getPassword()))
                 .studyCreatedByWeb(true)
                 .studyEnrollmentResultByWeb(true)
                 .studyUpdatedByWeb(true)
--- src/test/java/com/example/studyhyuck/account/AccountControllerTest.java
+++ src/test/java/com/example/studyhyuck/account/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.studyhyuck.account;
 
+import com.example.studyhyuck.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -82,7 +83,9 @@ class AccountControllerTest {
                 .andExpect(view().name("redirect:/"))
         ;
 
-        assertTrue(accountRepository.existsByEmail("lhg1304@naver.com"));
+        Account account = accountRepository.findByEmail("lhg1304@naver.com");
+        assertNotNull(account);
+        assertNotEquals(account.getPassword(), "12345678");
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 
