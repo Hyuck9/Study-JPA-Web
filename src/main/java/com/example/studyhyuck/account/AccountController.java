71b130331a4767f5e3a97fd64b27718761cb59d7
Hyuck <lhg1304@gmail.com>
2021-03-28 17:26:38 +0900

    회원 가입 리팩토링 및 테스트

--- src/main/java/com/example/studyhyuck/account/AccountController.java
+++ src/main/java/com/example/studyhyuck/account/AccountController.java
@@ -1,6 +1,5 @@
 package com.example.studyhyuck.account;
 
-import com.example.studyhyuck.ConsoleMailSender;
 import com.example.studyhyuck.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
@@ -20,8 +19,7 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -40,25 +38,7 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODO: encoding 해야함
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdatedByWeb(true)
-                .build();
-
-        Account newAccount = accountRepository.save(account);
-
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("StudyHyuck, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
-
-        javaMailSender.send(mailMessage);
-        // TODO: 회원 가입 처리
+        accountService.processNewAccount(signUpForm);
         return "redirect:/";
     }
 
--- src/test/java/com/example/studyhyuck/account/AccountControllerTest.java
+++ src/test/java/com/example/studyhyuck/account/AccountControllerTest.java
@@ -5,9 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -15,16 +23,67 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class AccountControllerTest {
 
-    @Autowired private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @MockBean
+    JavaMailSender javaMailSender;
+
+
     @DisplayName("회원 가입 화면 보이는지 테스트")
     @Test
     public void signUpForm() throws Exception {
-        this.mockMvc.perform(get("/sign-up"))
+        this.mockMvc
+                .perform(
+                        get("/sign-up")
+                )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("account/sign-up"))
                 .andExpect(model().attributeExists("signUpForm"))
         ;
+    }
+    
+    @DisplayName("회원 가입 처리 - 입력값 오류")
+    @Test
+    public void signUpSubmit_with_wrong_input() throws Exception {
+        
+        this.mockMvc
+                .perform(
+                        post("/sign-up")
+                                .param("nickname", "hyuck9")
+                                .param("email", "email..")
+                                .param("password", "12345")
+                                .with(csrf())
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"))
+        ;
+    }
+
+    @DisplayName("회원 가입 처리 - 입력값 정상")
+    @Test
+    public void signUpSubmit_with_correct_input() throws Exception {
+
+        this.mockMvc
+                .perform(
+                        post("/sign-up")
+                                .param("nickname", "hyuck9")
+                                .param("email", "lhg1304@naver.com")
+                                .param("password", "12345678")
+                                .with(csrf())
+                )
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"))
+        ;
+
+        assertTrue(accountRepository.existsByEmail("lhg1304@naver.com"));
+        then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 
 }
