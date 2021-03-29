b78170cf4182670cb62d914f00cd676f52baabac
Hyuck <lhg1304@nexmore.co.kr>
2021-03-29 18:37:39 +0900

    패스워드 수정 테스트

--- src/test/java/com/example/studyhyuck/settings/SettingsControllerTest.java
+++ src/test/java/com/example/studyhyuck/settings/SettingsControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
@@ -36,6 +37,9 @@ class SettingsControllerTest {
 
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void beforeEach() {
@@ -125,6 +129,61 @@ class SettingsControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("account"))
                 .andExpect(model().attributeExists("profile"))
+        ;
+    }
+
+    @WithAccount(value = "lhg1304")
+    @DisplayName("패스워드 수정 폼")
+    @Test
+    public void updatePasswordForm() throws Exception {
+        this.mockMvc
+                .perform(
+                        get(SettingsController.SETTINGS_PASSWORD_URL)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("passwordForm"))
+        ;
+    }
+
+    @WithAccount(value = "lhg1304")
+    @DisplayName("패스워드 수정 - 입력값 정상")
+    @Test
+    public void updatePasswordSuccess() throws Exception {
+        this.mockMvc
+                .perform(
+                        post(SettingsController.SETTINGS_PASSWORD_URL)
+                                .param("newPassword", "12345678")
+                                .param("newPasswordConfirm", "12345678")
+                                .with(csrf())
+                )
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingsController.SETTINGS_PASSWORD_URL))
+                .andExpect(flash().attributeExists("message"))
+        ;
+        Account lhg1304 = accountRepository.findByNickname("lhg1304");
+        assertTrue(passwordEncoder.matches("12345678", lhg1304.getPassword()));
+    }
+
+    @WithAccount(value = "lhg1304")
+    @DisplayName("패스워드 수정 - 입력값 에러 - 패스워드 불일치")
+    @Test
+    public void updatePasswordFail() throws Exception {
+        this.mockMvc
+                .perform(
+                        post(SettingsController.SETTINGS_PASSWORD_URL)
+                                .param("newPassword", "12345678")
+                                .param("newPasswordConfirm", "1234567890")
+                                .with(csrf())
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PASSWORD_VIEW_NAME))
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("passwordForm"))
         ;
     }
 
