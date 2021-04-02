d09ce72b11edb172b3fabb098680938f9fa0555e
Hyuck <lhg1304@gmail.com>
2021-04-03 02:05:18 +0900

    지역 도메인 생성

---
+++ src/main/java/com/example/studyhyuck/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.example.studyhyuck.zone;
+
+import com.example.studyhyuck.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
