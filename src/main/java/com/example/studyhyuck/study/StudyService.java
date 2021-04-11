a78b911d66dc58bce7f015f6691bfaaae9aeb3ab
Hyuck <lhg1304@gmail.com>
2021-04-11 17:46:12 +0900

    스터디 설정 - 배너

--- src/main/java/com/example/studyhyuck/study/StudyService.java
+++ src/main/java/com/example/studyhyuck/study/StudyService.java
@@ -43,4 +43,16 @@ public class StudyService {
     public void updateStudyDescription(Study study, StudyDescriptionForm studyDescriptionForm) {
         modelMapper.map(studyDescriptionForm, study);   // Update 쿼리 발생
     }
+
+    public void updateStudyImage(Study study, String image) {
+        study.setImage(image);
+    }
+
+    public void enableStudyBanner(Study study) {
+        study.setUseBanner(true);
+    }
+
+    public void disableStudyBanner(Study study) {
+        study.setUseBanner(false);
+    }
 }
