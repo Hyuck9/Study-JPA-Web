ecfe515ee93ca45b3cada27ff7c143d3d9b38619
Hyuck <lhg1304@gmail.com>
2021-04-11 19:11:27 +0900

    스터디 참여 및 탈퇴, 테스트

---
+++ src/test/java/com/example/studyhyuck/account/AccountFactory.java
@@ -0,0 +1,23 @@
+package com.example.studyhyuck.account;
+
+import com.example.studyhyuck.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AccountFactory {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public Account createAccount(String nickname) {
+        Account account = new Account();
+        account.setNickname(nickname);
+        account.setEmail(nickname + "@email.com");
+        accountRepository.save(account);
+        return account;
+    }
+
+}
