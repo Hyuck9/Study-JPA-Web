e4e01a11c3ac4ba8254174d87a74aeaa29d8b296
Hyuck <lhg1304@gmail.com>
2021-04-17 14:28:10 +0900

    모임 목록 조회 기능 구현

--- src/main/java/com/example/studyhyuck/event/EventRepository.java
+++ src/main/java/com/example/studyhyuck/event/EventRepository.java
@@ -1,9 +1,17 @@
 package com.example.studyhyuck.event;
 
 import com.example.studyhyuck.domain.Event;
+import com.example.studyhyuck.domain.Study;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional(readOnly = true)
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @EntityGraph(value = "Event.withEnrollments", type = EntityGraph.EntityGraphType.LOAD)
+    List<Event> findByStudyOrderByStartDateTime(Study study);
+
 }
